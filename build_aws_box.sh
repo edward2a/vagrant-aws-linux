043d1652b2906afbe513e04d60860c7c0fff423d
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-02-20 22:14:53 +0000

    update pkg path again, this time it matches :)

--- build_aws_box.sh
+++ build_aws_box.sh
@@ -107,7 +107,7 @@ gunzip --keep -c temp/box.img.gz | VBoxManage convertfromraw stdin box/box-disk1
 
 echo "INFO: Preparing import files..."
 mkdir realreadme
-tar -czf realreadme/amazon2016-09.pkg -C box $(ls box)
+tar -czf realreadme/amazon2016.09.pkg -C box $(ls box)
 sha1_sum=$(sha1sum realreadme/amazon-2016-09.pkg | awk '{ print $1 }')
 sed -e "s/@@@CHECKSUM@@@/${sha1_sum}/" -e "s|@@@IMPORT_PATH@@@|${PWD}|" import_metadata.json > import.json
 
