6ae494d003ff29e4f9cdf07f066ae62e01eff909
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-02-20 22:28:34 +0000

    add aws cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo "INFO: Removing instances"
+vagrant destroy -f
+
+# wait for instances to be terminated
+echo "INFO: Waiting 30s for instances to be terminated"
+sleep 30s
+
+echo "INFO: Removing ec2 key pair"
+[ -f "temp/ssh_key.id" ] && aws ec2 delete-key-pair --key-name $(cat temp/ssh_key.id)
+
+echo "INFO: Removing ec2 security group"
+[ -f "temp/group.name" ] && aws ec2 delete-security-group --group-name $(cat temp/group.name)
+
 rm -rf import.json realreadme/ temp/ box/box-disk1.vmdk 
