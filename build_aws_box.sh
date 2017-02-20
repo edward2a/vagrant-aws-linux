2b3d117d4f62f819c8df787f53807fd5ad61b3fd
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-02-20 21:38:52 +0000

    minor fixes

--- build_aws_box.sh
+++ build_aws_box.sh
@@ -103,12 +103,12 @@ vagrant halt i_export
 # THE DYNAMIC IMAGE SIZE IS HARDCODED TO 8GiB #
 ###############################################
 echo "INFO: Decompressing image and converting to VirtualBox format"
-gunzip --keep -c temp/box.img.gz | VBoxManage convertfromraw stdin temp/box-disk1.vmdk 8589934592 --format VMDK
+gunzip --keep -c temp/box.img.gz | VBoxManage convertfromraw stdin box/box-disk1.vmdk 8589934592 --format VMDK
 
 echo "INFO: Preparing import files..."
 mkdir realreadme
 tar -czf realreadme/amazon-2016-09.pkg -C box $(ls box)
-sha1_sum=$(sha1sum amazon-2016-09.box | awk '{ print $1 }')
+sha1_sum=$(sha1sum realreadme/amazon-2016-09.pkg | awk '{ print $1 }')
 sed -e "s/@@@CHECKSUM@@@/${sha1_sum}/" -e "s/@@@IMPORT_PATH@@@/$PWD/" import_metadata.json > import.json
 
 echo "INFO: Box export process finished"
