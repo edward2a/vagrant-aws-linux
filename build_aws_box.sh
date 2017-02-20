06ae303ad07dfd3d05c9e3518d0dd505dffdca53
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-02-20 23:42:39 +0000

    change .pkg to .box to match atlas requirement

--- build_aws_box.sh
+++ build_aws_box.sh
@@ -107,8 +107,8 @@ gunzip --keep -c temp/box.img.gz | VBoxManage convertfromraw stdin box/box-disk1
 
 echo "INFO: Preparing import files..."
 mkdir realreadme
-tar -czf realreadme/amazon2016.09.pkg -C box $(ls box)
-sha1_sum=$(sha1sum realreadme/amazon2016.09.pkg | awk '{ print $1 }')
+tar -czf realreadme/amazon2016.09.box -C box $(ls box)
+sha1_sum=$(sha1sum realreadme/amazon2016.09.box | awk '{ print $1 }')
 sed -e "s/@@@CHECKSUM@@@/${sha1_sum}/" -e "s|@@@IMPORT_PATH@@@|${PWD}|" import_metadata.json > import.json
 
 echo "INFO: Box export process finished"
