5b91d5a9857374a2c51195e49c22b415e893d8b6
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-02-23 19:31:38 +0000

    add removal of image source volume

--- cleanup.sh
+++ cleanup.sh
@@ -13,4 +13,7 @@ echo "INFO: Removing ec2 key pair"
 echo "INFO: Removing ec2 security group"
 [ -f "temp/group.name" ] && aws ec2 delete-security-group --group-name $(cat temp/group.name)
 
+echo "INFO: Removing image source volume"
+[ -f "temp/export_vol.id" ] && aws ec2 delete-volume --volume-id $(cat temp/export_vol.id)
+
 rm -rf import.json realreadme/ temp/ box/box-disk1.vmdk 
